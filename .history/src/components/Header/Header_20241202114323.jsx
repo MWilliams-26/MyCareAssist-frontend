5c980d2b58d2896c393795713ce1ac45ea92c208
Michael <mow26@icloud.com>
2024-12-02 14:28:19 -0500

    update/upload new fonts

---
+++ .history/src/components/Header/Header_20241202114323.jsx
@@ -0,0 +1,28 @@
+import logoNoBackGround from "../../assets/logoNoBackGround.svg"
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo-container">
+          <h1 className="header__logo">
+            MyCareAssist
+          </h1>
+        </div>
+        <button className="header__profile-btn">
+          MyCarePage
+        </button>
+        <button className="header__emergency-btn"></button>
+        <button className="header__register-btn">
+          Sign Up
+        </button>
+        <button className="header__login-btn">
+          Log In
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ .history/src/components/Header/Header_20241202114548.jsx
@@ -0,0 +1,36 @@
+import logoNoBackGround from "../../assets/logoNoBackGround.svg"
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo-container">
+          <h1 className="header__logo">
+            MyCareAssist
+          </h1>
+        </div>
+        <button className="header__profile-btn">
+          MyCarePage
+        </button>
+        <button className="header__emergency-btn">
+          Emergency Contacts
+        </button>
+        <button className="header__doctors-btn">
+          Doctors
+        </button>
+        <button className="header__calendar-btn">
+          Calendar
+        </button>
+        <button className="header__register-btn">
+          Sign Up
+        </button>
+        <button className="header__login-btn">
+          Log In
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
