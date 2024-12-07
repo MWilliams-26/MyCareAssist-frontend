57200613e658efe377bc981d4455a092c9f478aa
Michael <mow26@icloud.com>
2024-12-07 01:28:03 -0500

    working on calendar component

---
+++ .history/src/components/Dashboard/Dashboard_20241206235814.jsx
@@ -0,0 +1,33 @@
+import SetupProfile from "../SetupProfileModal/SetupProfileModal";
+import DoctorsModal from "../DoctorsModal/DoctorsModal";
+import EmergencyContacts from "../EmergencyContactsModal/EmergencyContacts";
+function Dashboard({ }) {
+  return (
+    <div className="dashboard">
+      <section className="dashboard__header">
+        <h1>My Care Assist</h1>
+        <p>Welcome to your dashboard!</p>
+      </section>
+      <section className="dashboard__hub">
+        <h2>Your Hub</h2>
+        <div className="dashboard__hub-container">
+        {}
+          <div className="dashboard__hub-profile">
+            <SetupProfile />
+          </div>
+          <div className="dashboard__hub-calendar"></div>                                                                                                                                                                                                                                                                
+          <div className="dashboard__hub-doctor">
+            <DoctorsModal />
+          </div>
+          <div className="dashboard__hub-emergency">
+            <EmergencyContacts />
+          </div>
+          <div className="dashboard__hub-medication"></div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Dashboard;
+
