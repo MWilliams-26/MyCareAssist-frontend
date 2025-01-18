5d9676fe11ee0b8264d9fb6dc425f6321473665c
Michael <mow26@icloud.com>
2025-01-18 16:05:21 -0500

    trying to restore

--- .history/src/utils/calender.config_20250118160420.js
+++ .history/src/utils/calender.config_20250118160420.js
@@ -12,9 +12,9 @@
 //   SCOPES: import.meta.env.VITE_GOOGLE_CALENDAR_SCOPES,
 // };
 
-export const GOOGLE_CALENDAR_CONFIG = {
-  CLIENT_ID: "273881584331-8ri21gc1og6tfu3l1r3v7na4fkc17452.apps.googleusercontent.com",
-  API_KEY: "AIzaSyBcYJlJvPX-KAh9VuXYbJsifqbKtLdFiUI",
-  CALENDAR_ID: "primary",
-  SCOPES: "https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar"
-}
+// export const GOOGLE_CALENDAR_CONFIG = {
+//   CLIENT_ID: "273881584331-8ri21gc1og6tfu3l1r3v7na4fkc17452.apps.googleusercontent.com",
+//   API_KEY: "AIzaSyBcYJlJvPX-KAh9VuXYbJsifqbKtLdFiUI",
+//   CALENDAR_ID: "primary",
+//   SCOPES: "https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar"
+// }
