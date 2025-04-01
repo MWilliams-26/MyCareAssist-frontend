6d217c4e3b0beac2ac506b4835b7885012e02e6f
Michael <mow26@icloud.com>
2025-04-01 12:55:12 -0400

    merging conflicts from stage-1-frontend-api to main branch

---
+++ .history/src/main_20250401125216.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import GoogleOAuthProvider
+import App from "./components/App/App";
+import './index.css';
+
+const clientId = "273881584331-8ri21gc1og6tfu3l1r3v7na4fkc17452.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/MyCareAssist-frontend/">
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
+
